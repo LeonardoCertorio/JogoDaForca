318c0476a6a4ec6ffa260cff1f1c218d36962a13
Leonardo <leonardo.certorio@gmail.com>
2025-02-17 23:09:55 -0300

    000002
    - Ajustes no código;
    - Verificação de inputs nulos e múltiplos caracteres;

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Linq.Expressions;
 using System.Runtime.ConstrainedExecution;
 
 class Program {
+    static private int difficultNumber;
+    static private char letter;
     static private string tracejado = "";
     static private string[] easyWords = ["MACACO", "CELULAR", "BRASILEIRO", "CADEIRA"];
     static private string[] mediumWords = ["CABECEIO", "CAPIVARA", "CHINES", "ZEBRA"];
@@ -15,17 +17,35 @@ class Program {
         Console.WriteLine("2 - Medium");
         Console.WriteLine("3 - Hard");
 
-        if (Console.ReadLine)
-        int difficultNumber = int.Parse(Console.ReadLine());
+        if (Console.ReadLine != null)
+            difficultNumber = int.Parse(Console.ReadLine());
 
-        DrawLayout(GetWord(difficultNumber));      
+        Console.WriteLine(DrawLayout(GetWord(difficultNumber))+"\n\n");
+
+        if(IsNullOrWhiteSpace(Console.ReadLine())) {
+            Console.WriteLine("Please, write some character!");    
+        }
+
+        while (Console.ReadLine().Length != 1) {
+            Console.WriteLine("Please, one letter by time!");
+        }
+
+        letter = char.Parse(Console.ReadLine());
+
+        //Todo: Implementar verificação se a letra está contida na string
+
+        //Todo: (Caso esteja contida) Implementar verificação do local da letra na string
+
+        //Todo: (Caso não esteja contida) Implementar "perca de vida" e inserir loop para número "x" de tentativas
 
     }
 
-    static void DrawLayout(string pWord) {
+    static string DrawLayout(string pWord) {
         for (int i=0; i != pWord.Length; i++) {
-            tracejado += "_";
+            tracejado += "_ ";
         }
+
+        return tracejado;
     }
 
     static string GetWord(int pIndex) {
@@ -48,6 +68,14 @@ class Program {
         }       
 
         return ChoosedWord;
+    }
+
+    static Boolean IsNullOrWhiteSpace(string pInput) {
+        if(pInput == null || pInput == "") {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
